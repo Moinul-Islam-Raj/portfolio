e1684639c29e1999fa8c573dd773dd515de54919
Moinul Islam Raj <moinulislamraj0@gmail.com>
2023-08-05 00:09:54 +0600

    security email change

--- src/data/owner.js
+++ src/data/owner.js
@@ -1,6 +1,6 @@
 export const owner = {
   name: 'Moinul Islam Raj',
-  email: 'moinulislamraj0@gmail.com',
+  email: 'moinulislammail1@gmail.com',
   facebook: 'https://www.facebook.com/69Raj69/',
   github: 'https://www.github.com/Moinul-Islam-Raj/',
   logoUrl: './logo.png',
